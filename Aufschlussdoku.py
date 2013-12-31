261bffe80cfd2cf99662b448f5144c17034431f6
Dieter Riegler <dieter.riegler@gmail.com>
2013-12-31 17:07:21 +0100

    add some details (csv-name at the end, script and version, save as csv-name.sla)

--- Aufschlussdoku.py
+++ Aufschlussdoku.py
@@ -25,10 +25,11 @@ def main(argv):
     #########################
     #  YOUR CODE GOES HERE  #
     #########################
+    script = "Aufschlussdoku.py"
+    version = "20131231"
     csvfile = scribus.fileDialog("csv2table :: open file", "*.csv")
-    #pos_filename_l = csvfile.rfind("/")
-    #filename = csvfile.rstrip(pos_filename_l)
-    #print filename
+    fname_ext = csvfile[csvfile.rfind("/") + 1:]
+    fname_noext = fname_ext[:fname_ext.rfind(".")]
     reader = csv.reader(open(csvfile, "rb"), delimiter=";")
     boxes = [["strat_d", 2.25], ["strat_n", 2.25], ["strat_b", 6.5], ["strat_p", 2.3], ["strat_a", 3.9]]
     start_x = float(1.9075)
@@ -41,16 +42,16 @@ def main(argv):
             strat_ct = strat_ct + 1
             strat_d = line[0].replace(",", ".");
             strat_uk = float(strat_d) + float(strat_ok)
-            print strat_uk
+            #print strat_uk
             strat_uk_txt = str(strat_uk).replace(".", ",");
-            print strat_uk_txt
+            #print strat_uk_txt
             strat_h = line[0]
             strat_n = line[1]
             strat_b = line[2]
             strat_p = line[3]
             strat_a = line[4]
             strat = [strat_d, strat_uk, strat_h, strat_n, strat_b, strat_p, strat_a]
-            print strat
+            #print strat
             strats.append(strat)
             start_x_0 = start_x
             box_nr = int(0)
@@ -64,23 +65,29 @@ def main(argv):
                 #print box_n, box_txt_n, dimensions[1], position
                 scribus.createRect(start_x_0, start_y, float(box[1]), float(strat_d)*2.0, box_n)
                 scribus.setLineWidth(0.567, box_n)
+                scribus.sentToLayer("profil_rahmen", box_n)
                 if box_nr == 0:
                     scribus.createText(start_x_0 + 0.1, start_y + (float(strat_d) * 2.0) - 0.4, float(box[1]) - 0.2, 0.4, box_txt_n)
-                    print strat_uk_txt
+                    #print strat_uk_txt
                     scribus.setText(strat_uk_txt, box_txt_n)
                 else:
                     scribus.createText(start_x_0 + 0.1, start_y + 0.1, float(box[1]) - 0.2, (float(strat_d)*2.0) - 0.1, box_txt_n)
                     scribus.setText(box_txt, box_txt_n)
                 scribus.setStyle("Buchner_Standard", box_txt_n)
+                scribus.sentToLayer("profil_txt", box_txt_n)
                 start_x_0 = start_x_0 + float(box[1])
-                #print start_x_0, start_y, box[1], strat_d
                 box_nr = box_nr + 1
-            print "end: strat count:", strat_ct
+            #print "end: strat count:", strat_ct
             start_y = start_y + float(strat_d) * 2.0
             start_x_0 = start_x
             strat_ok = strat_ok + float(strat_d)
     print "end: all"
-    #scribus.saveDocAs("test.sla")
+    scribus.createText(start_x_0 + 0.1, start_y + 0.1, 17, 0.5, "box_txt_end")
+    scribus.sentToLayer("profil_txt", "box_txt_end")
+    endtext = "erstellt mit " + fname_ext + ", " + script + " (V" + version + ")"
+    scribus.setText(endtext, "box_txt_end")
+    scribus.setStyle("Buchner_sehrklein", "box_txt_end")
+    scribus.saveDocAs(fname_noext + ".sla")
 
 def main_wrapper(argv):
     """The main_wrapper() function disables redrawing, sets a sensible generic
